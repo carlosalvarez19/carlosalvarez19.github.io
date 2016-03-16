c402a233d85d7af8f621435350bd44685a05a6a1
carlosalvarez19 <parklink_oz@hotmail.com>
2016-03-16 11:23:02 -0600

    Update 160316-texturas2.js

--- rv/160316-texturas2.js
+++ rv/160316-texturas2.js
@@ -28,7 +28,7 @@ function setup() {
   
   
   renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.InnerHeight*.95, window.InnerHeight*.95 );
+  renderer.setSize( window.innerHeight*.95, window.innerHeight*.95 );
   document.body.appendChild( renderer.domElement );
 }
   
