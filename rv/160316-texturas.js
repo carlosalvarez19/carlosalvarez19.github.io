75790ea2363a9d9525a5d35371491530ceafd917
carlosalvarez19 <parklink_oz@hotmail.com>
2016-03-16 10:50:40 -0600

    Update 160316-texturas.js

--- rv/160316-texturas.js
+++ rv/160316-texturas.js
@@ -1,18 +1,18 @@
 function setup() { 
   THREE.ImageUtils.crossOrigin = '';
   var   textura   = THREE.ImageUtils.loadTexture(´http://threejs.org/examples/textures/create.gif');
-  var   material  = THREE.MeshBasicmaterial( {map: textura} );
-  var   forma     = new THREE.Box Geometry( 1, 1, 1 );
+  var   material  = new THREE.MeshBasicMaterial( {map: textura} );
+  var   forma     = new THREE.BoxGeometry( 1, 1, 1 );
   malla           = new THREE.Mesh( forma, material );
   
   escena = new THREE.Scene();
   escena.add(malla);
   
-  camara = new THREE.PerspectiveCamerea();
+  camara = new THREE.PerspectiveCamera();
   camara.position.z = 5;
   
-  renderer = new THREE.webGLRenderer();
-  renderer.SetSize( window.InnerHeight*.95, window.InnerHeight*.95 );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.InnerHeight*.95, window.InnerHeight*.95 );
   document.body.appendChild( renderer.domElemenet );
 }
   
