546d0084381210efc3f074a5183ce60919212cdc
carlosalvarez19 <parklink_oz@hotmail.com>
2016-04-03 23:08:56 -0500

    Update tarea robot2.js

--- rv/tarea robot2.js
+++ rv/tarea robot2.js
@@ -1,153 +1,198 @@
-function Cabeza (){
-   
+function Brazo (){
+
 THREE.Object3D.call(this);
-    
-this.ojo1 = new THREE.Mesh( new THREE.CylinderGeometry(2,2,5,100) );
-this.ojo2 = new THREE.Mesh( new THREE.CylinderGeometry(2,2,5,100) );
-this.cuello = new THREE.Mesh( new THREE.CylinderGeometry(1,1,5,100) );
-this.ojo1.position.y = 10;
-this.ojo2.position.y = 10;
-this.ojo1.position.x = -2;
-this.ojo2.position.x = 2;
-this.cuello.position.y = 7;
-this.add(this.cuello);
-this.add(this.ojo1);
-this.add(this.ojo2);
+this.brazo=new THREE.Mesh(new THREE.BoxGeometry(20,20,120));
+
+  
+  var gripper =new THREE.Shape();
+  gripper.moveTo(0,0);
+  gripper.lineTo(0,20);
+  gripper.lineTo(20,40);
+  gripper.lineTo(40,40);
+  gripper.lineTo(60,20);
+  gripper.lineTo(60,15);
+  gripper.lineTo(50,15);
+  gripper.lineTo(50,20);
+  gripper.lineTo(40,30);
+  gripper.lineTo(20,30);
+  gripper.lineTo(10,20);
+  gripper.lineTo(10,0);
+  gripper.lineTo(20,-10);
+  gripper.lineTo(40,-10);
+  gripper.lineTo(50,0);
+  gripper.lineTo(50,5);
+  gripper.lineTo(60,5);
+  gripper.lineTo(60,0);
+  gripper.lineTo(40,-20);
+  gripper.lineTo(20,-20);
+  gripper.lineTo(0,0);
+  gripperForma = new THREE.ExtrudeGeometry(gripper,{amount:15});
+  this.griper = new THREE.Mesh(gripperForma);
+
+  this.brazo.position.x=60;
+  this.brazo.position.y=20;
+  this.brazo.position.z=70;
+  
+  this.griper.rotation.y=-1.57;
+  this.griper.position.x=70;
+  this.griper.position.y=10;    
+  this.griper.position.z=120;	
+
+this.add(this.brazo);
+this.add(this.griper);
 
 }
 
-Cabeza.prototype = new THREE.Object3D();
+
+function Brazo2 (){
+
+THREE.Object3D.call(this);
+this.brazo=new THREE.Mesh(new THREE.BoxGeometry(20,20,120));
+
+  
+  var gripper =new THREE.Shape();
+  gripper.moveTo(0,0);
+  gripper.lineTo(0,20);
+  gripper.lineTo(20,40);
+  gripper.lineTo(40,40);
+  gripper.lineTo(60,20);
+  gripper.lineTo(60,15);
+  gripper.lineTo(50,15);
+  gripper.lineTo(50,20);
+  gripper.lineTo(40,30);
+  gripper.lineTo(20,30);
+  gripper.lineTo(10,20);
+  gripper.lineTo(10,0);
+  gripper.lineTo(20,-10);
+  gripper.lineTo(40,-10);
+  gripper.lineTo(50,0);
+  gripper.lineTo(50,5);
+  gripper.lineTo(60,5);
+  gripper.lineTo(60,0);
+  gripper.lineTo(40,-20);
+  gripper.lineTo(20,-20);
+  gripper.lineTo(0,0);
+  gripperForma = new THREE.ExtrudeGeometry(gripper,{amount:15});
+  this.griper = new THREE.Mesh(gripperForma);
+
+  this.brazo.position.x=-60;
+  this.brazo.position.y=20;
+  this.brazo.position.z=70;
+  
+  this.griper.rotation.y=-1.57;
+  this.griper.position.x=-55;
+  this.griper.position.y=10;    
+  this.griper.position.z=120;	
+
+this.add(this.brazo);
+this.add(this.griper);
+
+}
 
 
-function setup() {
-  
-    var tri= new THREE.Shape();
-    tri.moveTo(0,0);
-    tri.lineTo(2,2);
-    tri.lineTo(4,0);
-    tri.lineTo(0,0);
-    var trianguloForma = new THREE.ExtrudeGeometry(tri,{amount:-5});
-  
-    var cuerpoForma = new THREE.Mesh( new THREE.BoxGeometry(10,10,10) );
-    cabeza = new Cabeza();
-    cuerpo.position.y = 2;
-    piernaD.position.z = -1;
-    piernaI.position.z = 1;
-    step = 0.1;
-    escena = new THREE.Scene();
-    escena.add( cuerpo );
-    escena.add( piernaD );
-    escena.add( piernaI );
-    camara = new THREE.PerspectiveCamera();
-    camara.position.z = 20;
-    
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerHeight*.95,window.innerHeight*.95 );
-    document.body.appendChild( renderer.domElement );
-}
-function loop(){
-    requestAnimationFrame( loop );
-    renderer.render( escena, camara );    
-    if ( Math.abs(piernaD.rotation.z) > .5 ) 
-        step = -step;
-      piernaD.rotation.z += step;
-      piernaI.rotation.z -= step;
-}
-  
-var escena, camara, renderer;
-var step, piernaI, piernaD;
+function Cabeza (){
+
+THREE.Object3D.call(this);
+
+this.cilindro1=new THREE.Mesh(new THREE.CylinderGeometry(20,20,50,100));
+this.cilindro2=new THREE.Mesh(new THREE.CylinderGeometry(20,20,50,100));
+this.cilindro3=new THREE.Mesh(new THREE.CylinderGeometry(10,10,50,100));
+
+ this.cilindro1.position.y=100;
+ this.cilindro1.position.x=-20;
+ this.cilindro1.rotation.x +=1.57;
  
-setup();
-loop();
+ this.cilindro2.position.y=100;
+ this.cilindro2.position.x=20;
+ this.cilindro2.rotation.x +=1.57;
+
+ this.cilindro3.position.y=70;
+
+ this.add(this.cilindro1); 
+ this.add(this.cilindro2);
+ this.add(this.cilindro3);
+}
 
 
+Brazo.prototype=new THREE.Object3D();
+Brazo2.prototype=new THREE.Object3D();
+Cabeza.prototype=new THREE.Object3D();
 
 function setup(){
 
-var cuboForma= 
-var brazoForma= new THREE.BoxGeometry(2,2,12);
-var cilindroForma= new THREE.CylinderGeometry(2,2,5,100);
-var cilindroForma2= new THREE.CylinderGeometry(1,1,5,100);
+var cuboForma=new THREE.BoxGeometry(100,100,100);
+var tri= new THREE.Shape();
+tri.moveTo(0,0);
+tri.lineTo(50,50);
+tri.lineTo(100,0);
+tri.lineTo(0,0);
+var trianguloForma = new THREE.ExtrudeGeometry(tri,{amount:-50});
+
 
   var cubo = new THREE.Mesh(cuboForma);
-  var brazo1= new THREE.Mesh(brazoForma);
-  var brazo2= new THREE.Mesh(brazoForma);
-  var cilindro1 = new THREE.Mesh(cilindroForma);
-  var cilindro2 = new THREE.Mesh(cilindroForma);
-  var cilindro3 = new THREE.Mesh(cilindroForma2);
   var triangulo1 = new THREE.Mesh(trianguloForma);
   var triangulo2 = new THREE.Mesh(trianguloForma);
- 
- cilindro1.position.y=10;
- cilindro1.position.x=-2;
- cilindro1.rotation.x +=1.57;
- 
- cilindro2.position.y=10;
- cilindro2.position.x=2;
- cilindro2.rotation.x +=1.57;
+  
 
- cilindro3.position.y=7;
- 
- triangulo1.position.x=10;
- triangulo1.position.y=-7;
+ triangulo1.position.x=100;
+ triangulo1.position.y=-70;
+ triangulo1.position.z=50;
  triangulo1.rotation.y +=1.57;
 
- triangulo2.position.x=-5;
- triangulo2.position.y=-7;
+ triangulo2.position.x=-50;
+ triangulo2.position.y=-70;
+ triangulo2.position.z=50;
  triangulo2.rotation.y +=1.57
 
- brazo1.position.x=5.5;
- brazo1.position.y=2;
- brazo1.position.z=6;
-
- brazo2.position.x=-5.5;
- brazo2.position.y=2;
- brazo2.position.z=6;
  
  var forma = new THREE.Geometry();
   
  THREE.GeometryUtils.merge(forma, cubo);
- THREE.GeometryUtils.merge(forma, brazo1);
- THREE.GeometryUtils.merge(forma, brazo2);
- THREE.GeometryUtils.merge(forma, cilindro1);
- THREE.GeometryUtils.merge(forma, cilindro2);
- THREE.GeometryUtils.merge(forma, cilindro3);
  THREE.GeometryUtils.merge(forma, triangulo1);
  THREE.GeometryUtils.merge(forma, triangulo2);
- 
+
  var material =new THREE.MeshNormalMaterial();
  malla= new THREE.Mesh(forma, material);
  
+ brazo1= new Brazo();
+ brazo2= new Brazo2();
+ cabeza= new Cabeza();
+  
+ //brazo2.position.x=-120;
+ 
+ step=0.05; 
+ step2=0.01;
+
  escena= new THREE.Scene();
  escena.add(malla);
+ escena.add(brazo1);
+ escena.add(brazo2);
+ escena.add(cabeza);
 
- //se inicializa la camara y el renderer
- 
  camara= new THREE.PerspectiveCamera();
- camara.position.z=50;
+ camara.position.z=500;
  
  renderer= new THREE.WebGLRenderer();
  renderer.setSize(window.innerHeight*.95, window.innerHeight*.95);
  document.body.appendChild(renderer.domElement);
  }
- 
- function loop() {
+
+function loop() {
   requestAnimationFrame(loop);
-  malla.rotation.y +=0.01;
-  
-  //for(var f=0; f<25; f=f+.01){
-  //malla.position.x += f;
-  }
-  
+  cabeza.rotation.y +=step2;
+  malla.rotation.y-=step2;
+  brazo1.rotation.y-=step2;
+  brazo2.rotation.y-=step2;
   renderer.render(escena,camara);
+if(Math.abs(brazo1.rotation.x)>.5)
+  step=-step;
+  brazo1.rotation.x+=step;
+  brazo2.rotation.x-=step;
   
-      if ( Math.abs(piernaD.rotation.z) > .25 ) 
-        step = -step;
-      brazo1.rotation.z += step;
-      brazo2.rotation.z -= step;
-      
 }
 
 var escena, camara, renderer, malla;
+var step, brazo1, brazo2, cabeza;
+
 setup();
 loop();
