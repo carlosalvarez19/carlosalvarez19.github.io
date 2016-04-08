bd708a1c7c9e39cb3625fe5d901d7c8160ffc5ba
carlosalvarez19 <parklink_oz@hotmail.com>
2016-04-08 17:29:13 -0500

    Update 300316-colisiones.js

--- rv/300316-colisiones.js
+++ rv/300316-colisiones.js
@@ -1,8 +1,8 @@
 function setup(){
   cubo1=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
   cubo2=new THREE.Mesh(new THREE.BoxGeometry(1,1,1),new THREE.MeshNormalMaterial());
-  cubo1.position.x=0.7;
-  cubo2.position.x=-0.7;
+  cubo1.position.x=0.5;
+  cubo2.position.x=-0.5;
   camara=new THREE.PerspectiveCamera();
   camara.position.z=5;
   
