cc6e0bb5cfd544eab7dcd5615211b42c491bfb5f
carlosalvarez19 <parklink_oz@hotmail.com>
2016-04-29 13:21:47 -0500

    Create cilindro sombra textura.js

---
+++ rv/cilindro sombra textura.js
@@ -0,0 +1,31 @@
+
+  var escena=new THREE.Scene();
+  var fov = 75; // Campo de vision
+  var aspect=window.innerWidth/window.innerHeight; //relacion de aspecto
+  var near=0.1;// plno cercano
+  var far =1000;// plano lejano
+  var camara = new THREE.PerspectiveCamera(fov, aspect, near, far );
+  
+  camara.position.z=5;
+  
+  var renderer=new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth,window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  
+  
+var forma = new THREE.CylinderGeometry(1,1,5);
+var material = new THREE.MeshDepthMaterial({color: 0x08f410});
+var cilindro = new THREE.Mesh(forma, material);
+escena.add(cilindro);
+  
+  function render(){
+    requestAnimationFrame(render);
+    
+    cilindro.rotation.x +=0.1;
+    renderer.render(escena,camara);
+  }
+  render();
+  
+</script>
+</body>
+</html>
